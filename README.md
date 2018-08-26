203f0bb1b5b9ae00c4354f79a735475dc82ff60a
golendercaria <>
2018-08-26 17:53:09 +0200

    page(front): Making presentation page
    Add one page for describe the theme features

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # wordpress_webpack_theme
 Simple Wordpress Webpack theme
+
+# configuration schema
+- webpack dev server:8080
+- apache + mysql for launch Wordpress:80
+
+# basic command
+> npm run dev
+> npm run prod
--- index.php
+++ index.php
@@ -2,7 +2,44 @@
 	get_header(); 
 ?>
 
-	<h1>Wordpress Webpack theme work !</h1>
+	<div class="d-grid grid-cols-12">
+		<div class="panel darkpurple col-12 align-center">
+			<h1>HOW IT WORK</h1>
+			<div class="require">npm is required !</div>
+		</div>
+		<div class="panel darkpurple col-12">
+			<ul>
+				<li>Add this theme in theme folder</li>
+				<li>Open terminal, and go in the theme directory</li>
+				<li>Execute "npm run dev"</li>
+			</ul>
+		</div>
+		<div class="panel yellow col-12">
+			<h2>FEATURES</h2>
+			<div class="d-grid grid-cols-12">
+				<div class="col-4 col-m-12 d-grid grid-cols-1 justify-self-center">
+					<h3>ES6 ready</h3>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/es6.png" />
+				</div>
+				<div class="col-4 col-m-12 d-grid grid-cols-1 justify-self-center">
+					<h3>SASS compiling</h3>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/responsive.png" />
+					
+				</div>
+				<div class="col-4 col-m-12 d-grid grid-cols-1 justify-self-center">
+					<h3>Auto reload style</h3>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/auto.png" />
+					
+				</div>
+				<div class="col-4 col-m-12 d-grid grid-cols-1 justify-self-center">
+					<h3>Module loader</h3>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/target.png" />
+
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 <?php 
 	get_footer(); 
